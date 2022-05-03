5788b2ff151c886ee2bb85184c4478802f8fc075
DDBC23 <danielbc23@hotmail.com>
2022-05-03 15:49:18 +0000

    Add new_game function and player inputs

--- run.py
+++ run.py
@@ -11,3 +11,26 @@ class GameArea:
         self.comp_guess = []
         self.player_guess = []
         self.ships = []
+
+
+def new_game():
+    """
+    Starts a new game instance. Sets player name, board size, number of ships
+    based on inputs and prints input values.
+    """
+    name = input("Please enter your name: ")
+    print(f"Welcome aboard {name}.\n")
+    try:
+        size = int(input("Select a board size between 5 & 10: "))
+        print(f"You have chosen a board size of {size}\n")
+    except ValueError:
+        print("Please enter a number between 5 & 10.\n")
+    try:
+        num_ships = int(input("Choose a number of ships per player between"
+                              " 1 & 10: "))
+        print(f"You have chosen {num_ships} ships per player.\n")
+    except ValueError:
+        print("Please choose a number between 1 & 10.\n")
+
+
+new_game()

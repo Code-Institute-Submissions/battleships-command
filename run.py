acb924d77f0e77c66ee4acc802e38b83c8069e81
DDBC23 <danielbc23@hotmail.com>
2022-05-03 14:22:00 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+class GameArea:
+    """
+    Constructs the game board, places battleships based on player input
+    and receives player name input.
+    """
+    playername = input("Please enter your name: ")
+    print(f"Welcome aboard {playername}")
+    boardsize = int(input("Choose a board size between 5 & 10: "))
+    print(f"You have chosen a board size of {boardsize}")
+
+    def __init__(self, playername, boardsize):
+        self.playername = playername
+        self.boardsize = boardsize
+
+    def board_setup(self):
+        """
+        Creates the board for player and computer based on board size
+        input
+        """
+        board = ""
+        for n in range(self.boardsize):
+            board_row = print(" - " * self.boardsize)
+            board_row.append(board)
+
+        return board
+
+    board_setup()
